548a75a01007da2ca5f84b42f81b5b66b60bb10b
ilker yılmaz <ilkerylmaz57@hotmail.com>
2025-08-28 21:20:00 +0300

    ses getiren yenilik

--- src/scripts/create-admin.ts
+++ src/scripts/create-admin.ts
@@ -1,7 +1,7 @@
 // src/scripts/generate-hash.ts
 import bcrypt from 'bcrypt';
 
-const password = 'admin123';
+const password = '1234';
 bcrypt.hash(password, 10).then(hash => {
     console.log('Kullanabileceğiniz hash:', hash);
 });
